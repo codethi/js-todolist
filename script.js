8aa3c831f7b39f6cc3329abf268bf27f935c480e
codethi <docencia.thiago@gmail.com>
2021-09-13 18:53:04 -0300

    comentários do arquivo JS

--- script.js
+++ script.js
@@ -10,24 +10,28 @@ list.addEventListener("click", function (ev) {
 const btnAdd = document.querySelector("#addBtn");
 
 btnAdd.addEventListener("click", function () {
-  const li = document.createElement("li");
-  const inputValue = document.querySelector("#myInput").value;
-  const taskText = document.createTextNode(inputValue);
-  li.appendChild(taskText);
-  
+  const li = document.createElement("li"); // Cria elemento li novo
+  const inputValue = document.querySelector("#myInput").value; // Pega texto digitado no input
+  const taskText = document.createTextNode(inputValue); // Add o texto do input na const taskText
+  li.appendChild(taskText); // Adiciona o texto na li
+
+  // Verifica se o input está vazio
   if (inputValue === "") {
     alert("Você deve digitar algum texto para adicionar!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("myUL").appendChild(li); // Add a li na ul
   }
-  document.querySelector("#myInput").value = "";
 
+  document.querySelector("#myInput").value = ""; // Zera o input
+
+  // Cria um btn de apagar para cada li, com um icon X
   const span = document.createElement("span");
-  const txt = document.createTextNode("\u00D7");
+  const icon = document.createTextNode("\u00D7");
   span.className = "close";
-  span.appendChild(txt);
+  span.appendChild(icon);
   li.appendChild(span);
-
+  
+  // Se o botão de apagar for clicado, ele muda o display da li para none, fazendo ela sumir da tela
   const close = document.getElementsByClassName("close");
   for (let i = 0; i < close.length; i++) {
     close[i].addEventListener("click", function () {
